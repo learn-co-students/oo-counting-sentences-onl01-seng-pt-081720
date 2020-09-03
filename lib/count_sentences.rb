b5759bd0b049fac34c98ab6cd090680f0cff3989
Ryan Altimari <ryanaltimari8@gmail.com>
2020-09-03 19:10:19 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,23 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
-  end
+    elements = ""
+    elements << self
+    seperate = elements.split(/[\.?!]/)
+    seperate.reject! { |e| e.to_s.empty? }
+    seperate.length
+   end
+   
 end
