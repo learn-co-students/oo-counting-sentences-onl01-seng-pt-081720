9d00a1efb2a60592341a8b1fd05c98a0166f84b3
Jeremy Wood <js.wood206@gmail.com>
2020-09-04 04:37:46 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    individual_sentences = []
+    individual_sentences = self.split(/[.?!]/)
+    individual_sentences.delete("")
+    individual_sentences.length
   end
 end
